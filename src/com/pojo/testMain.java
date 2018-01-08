253c50af3a4935ffd3ee613a9d9c3c8c1e7e5776
677jiayou <444718843@qq.com>
2018-01-08 15:10:11 +0800

    妈蛋到底能不能改

--- src/com/pojo/testMain.java
+++ src/com/pojo/testMain.java
@@ -3,10 +3,11 @@ package com.pojo;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 public class testMain {
     public static void main(String[] args) {
-        XmlBeanFactory
+        XmlBeanFactory xm=new XmlBeanFactory(new ClassPathResource("bean.xml"));
         //创建Spring上下文（加载bean.xml）
         ApplicationContext axt=new ClassPathXmlApplicationContext("bean.xml");
         //获取HelloWorld实例
