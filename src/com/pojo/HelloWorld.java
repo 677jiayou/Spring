b6e122e38b5caf6caca0a89dd173860dbfa16e1d
677jiayou <444718843@qq.com>
2018-01-08 15:55:57 +0800

    临时改一改

--- src/com/pojo/HelloWorld.java
+++ src/com/pojo/HelloWorld.java
@@ -3,6 +3,10 @@ package com.pojo;
 public class HelloWorld {
     String mes;
 
+    public String getMes() {
+        return mes;
+    }
+
     public void setMes(String mes) {
         this.mes = mes;
     }
--- src/com/pojo/testMain.java
+++ src/com/pojo/testMain.java
@@ -7,12 +7,14 @@ import org.springframework.core.io.ClassPathResource;
 
 public class testMain {
     public static void main(String[] args) {
-        XmlBeanFactory xm=new XmlBeanFactory(new ClassPathResource("bean.xml"));
         //创建Spring上下文（加载bean.xml）
         ApplicationContext axt=new ClassPathXmlApplicationContext("bean.xml");
         //获取HelloWorld实例
         HelloWorld hw=axt.getBean("HelloWorld",HelloWorld.class);
+        hw.setMes("hello 李佳奇！");
+        HelloWorld hw1=axt.getBean("HelloWorld",HelloWorld.class);
         //调用方法
         hw.say();
+        hw1.say();
     }
 }
