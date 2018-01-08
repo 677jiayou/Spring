f5c4d97332761495a9a983283ac3e95a0c15c5cf
677jiayou <444718843@qq.com>
2018-01-08 15:05:58 +0800

    临时改一改

--- src/com/pojo/testMain.java
+++ src/com/pojo/testMain.java
@@ -1,10 +1,12 @@
 package com.pojo;
 
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class testMain {
     public static void main(String[] args) {
+        XmlBeanFactory
         //创建Spring上下文（加载bean.xml）
         ApplicationContext axt=new ClassPathXmlApplicationContext("bean.xml");
         //获取HelloWorld实例
