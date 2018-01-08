d1c188a2bb420c64a7fa810c86f7c6af1104e1dd
677jiayou <444718843@qq.com>
2018-01-08 14:51:54 +0800

    Initial commit

---
+++ src/com/pojo/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.pojo;
+
+public class HelloWorld {
+    String mes;
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+    //HelloWorld的say()方法
+    public void say(){
+        System.out.println(mes);
+    }
+}
---
+++ src/com/pojo/testMain.java
@@ -0,0 +1,15 @@
+package com.pojo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class testMain {
+    public static void main(String[] args) {
+        //创建Spring上下文（加载bean.xml）
+        ApplicationContext axt=new ClassPathXmlApplicationContext("bean.xml");
+        //获取HelloWorld实例
+        HelloWorld hw=axt.getBean("HelloWorld",HelloWorld.class);
+        //调用方法
+        hw.say();
+    }
+}
